a68ff83bca861024c75da182447a96ad7b88a81b
Isaac Mwesigwa <mwesigwaisaac40@gmail.com>
2024-05-31 15:11:51 +0300

    added ellipses instead of bounding boxes

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ from utils import read_video,save_video
 from trackers import Tracker
 def main():
   # read video
-  video_frames = read_video('videos/08fd33_4.mp4')
+  video_frames = read_video('Analysis-System/videos/08fd33_4.mp4')
 
   # initialize tracker
-  tracker = Tracker('models/best.pt')
+  tracker = Tracker('Analysis-System/models/best.pt')
 
-  tracker.get_object_tracks(video_frames)
+  tracks = tracker.get_object_tracks(video_frames,
+                            read_from_stub=True,
+                            stub_path='Analysis-System/stubs/track_stubs.pk1')
+  
+  # draw output
+  ## draw object tracks
+  output_video_frames = tracker.draw_annotations(video_frames,tracks)
 
   # save video
-  save_video(video_frames,'output_videos/output.avi')
+  save_video(output_video_frames,'Analysis-System/output_videos/output_video.avi')
 
 if __name__ == '__main__':
   main()
+
+  
